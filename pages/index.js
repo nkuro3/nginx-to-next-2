089b6afa6654ec12ace514b5e16ece5bb870be8a
nkuro <63081563+nkuro3@users.noreply.github.com>
2024-07-18 14:09:12 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my NextJS app!</h1>
+      <p>This is a sample page served through Nginx and Docker.</p>
+    </div>
+  )
+}
